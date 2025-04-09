9201e9f48352afade87d5559a228e904a1fcd1dd
Tomis Hamilton <tomisnhamilton@gmail.com>
2025-04-09 11:14:43 -0600

    complete purge

--- app/+not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen() {
-    return (
-        <View style={{ padding: 20 }}>
-            <Text>Page not found.</Text>
-        </View>
-    );
-}
--- app/index.jsx
+++
@@ -1,9 +0,0 @@
-// app/index.tsx
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-    // Initially redirect to the accounts tab
-    return (
-        <Redirect href="/accounts" />
-    );
-}
